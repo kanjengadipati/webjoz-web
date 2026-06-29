11ce826a74d40bb2b122ddcdc38715595bfb8464
Heriyadi <adipatikanjeng@gmail.com>
2026-06-29 11:10:21 +0700

    fix: add openstreetmap.org to CSP frame-src to unblock map embed

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https: http:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://accounts.google.com https://*.facebook.com https://*.facebook.net; connect-src 'self' https: http: ws: wss:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://connect.facebook.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https: http:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://accounts.google.com https://*.facebook.com https://*.facebook.net https://www.openstreetmap.org; connect-src 'self' https: http: ws: wss:;",
           },
         ],
       },
