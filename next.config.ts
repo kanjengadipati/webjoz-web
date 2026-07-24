79e45213dfbc505b1f3abd8f51a4e753fe006e15
Heriyadi <adipatikanjeng@gmail.com>
2026-07-24 13:03:22 +0700

    fix: add production Midtrans domains to CSP
    Added snap-assets.midtrans.com, api.midtrans.com, and payment
    provider domains (Google Pay, GoPay, Google Tag Manager, Alibaba
    CDN) to Content-Security-Policy script-src and frame-src directives.

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://connect.facebook.net https://app.sandbox.midtrans.com https://app.midtrans.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; img-src 'self' blob: data: https: http:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://accounts.google.com https://*.facebook.com https://*.facebook.net https://www.openstreetmap.org https://app.sandbox.midtrans.com https://app.midtrans.com; connect-src 'self' https: http: ws: wss:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://connect.facebook.net https://app.sandbox.midtrans.com https://app.midtrans.com https://snap-assets.sandbox.midtrans.com https://snap-assets.midtrans.com https://api.sandbox.midtrans.com https://api.midtrans.com https://pay.google.com https://gwk.gopayapi.com https://www.googletagmanager.com https://o.alicdn.com https://g.alicdn.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com; img-src 'self' blob: data: https: http:; font-src 'self' data: https://fonts.gstatic.com; frame-src 'self' https://accounts.google.com https://*.facebook.com https://*.facebook.net https://www.openstreetmap.org https://app.sandbox.midtrans.com https://app.midtrans.com; connect-src 'self' https: http: ws: wss:;",
           },
         ],
       },
